079963723dc84d1fc851ad69aea24d9ac785bd9e
Sierra-034 <sierra_034@outlook.com>
2020-07-14 12:32:28 -0500

    project finished

---
+++ src/app/index.js
@@ -0,0 +1,28 @@
+require("./index.css");
+
+const { Weather } = require("./weather");
+const { UI } = require("./UI");
+const { Store } = require("./store");
+
+const store = new Store();
+const {city, countryCode} = store.getLocationData();
+
+const ui = new UI();
+const weather = new Weather(city, countryCode);
+
+async function fetchWeather() {
+    const data = await weather.getWeather();
+    console.log(data);
+    ui.render(data);
+}
+
+document.getElementById("w-change-btn").addEventListener("click", function(e) {
+    const city = document.getElementById("city").value;
+    const countryCode = document.getElementById("country-code").value;
+    weather.changeLocation(city, countryCode);
+    store.setLocationData(city, countryCode);
+    fetchWeather();
+    e.preventDefault();
+});
+
+document.addEventListener("DOMContentLoaded", fetchWeather);
---
+++ src/app/weather.js
@@ -0,0 +1,19 @@
+export class Weather {
+    constructor(city, countryCode) {
+        this.apikey = "cf3ce64208432b3020df844ed49c1b96";
+        this.city = city;
+        this.countryCode = countryCode;
+    }
+
+    async getWeather() {
+        const URI = `https://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.countryCode}&appid=${this.apikey}&units=metric`;
+        const response = await fetch(URI);
+        const data = await response.json();
+        return data;
+    }
+
+    changeLocation(city, countryCode) {
+        this.city = city;
+        this.countryCode = countryCode;
+    }
+}
